b6815ad48c46ea8af2aa4e30534a4e4c1dcafbd1
Solano-Nascimento <solanoomar@usp.br>
2025-09-25 16:28:03 -0300

    refactor: Update test cases to use 'calcular' function instead of 'calculadora'

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,43 +1,43 @@
 import unittest
-from calculadora import calculadora
+from calcular import calcular
 
-class TestCalculadora(unittest.TestCase):
+class TestCalcularcalcular(unittest.TestCase):
 
     def test_soma(self):
-        self.assertEqual(calculadora(5, 3, '+'), 8)
-        self.assertEqual(calculadora(-5, 3, '+'), -2)
-        self.assertEqual(calculadora(5.5, 2.5, '+'), 8.0)
+        self.assertEqual(calcular(5, 3, '+'), 8)
+        self.assertEqual(calcular(-5, 3, '+'), -2)
+        self.assertEqual(calcular(5.5, 2.5, '+'), 8.0)
 
     def test_subtracao(self):
-        self.assertEqual(calculadora(5, 3, '-'), 2)
-        self.assertEqual(calculadora(-5, 3, '-'), -8)
-        self.assertEqual(calculadora(5.5, 2.5, '-'), 3.0)
+        self.assertEqual(calcular(5, 3, '-'), 2)
+        self.assertEqual(calcular(-5, 3, '-'), -8)
+        self.assertEqual(calcular(5.5, 2.5, '-'), 3.0)
 
     def test_multiplicacao(self):
-        self.assertEqual(calculadora(5, 3, '*'), 15)
-        self.assertEqual(calculadora(-5, 3, '*'), -15)
-        self.assertEqual(calculadora(5.5, 2, '*'), 11.0)
+        self.assertEqual(calcular(5, 3, '*'), 15)
+        self.assertEqual(calcular(-5, 3, '*'), -15)
+        self.assertEqual(calcular(5.5, 2, '*'), 11.0)
 
     def test_divisao(self):
-        self.assertEqual(calculadora(6, 3, '/'), 2)
-        self.assertEqual(calculadora(-6, 3, '/'), -2)
-        self.assertEqual(calculadora(5, 2, '/'), 2.5)
+        self.assertEqual(calcular(6, 3, '/'), 2)
+        self.assertEqual(calcular(-6, 3, '/'), -2)
+        self.assertEqual(calcular(5, 2, '/'), 2.5)
 
     def test_divisao_por_zero(self):
-        self.assertIsNone(calculadora(5, 0, '/'))
+        self.assertIsNone(calcular(5, 0, '/'))
 
     def test_potenciacao(self):
-        self.assertEqual(calculadora(2, 3, '^'), 8)
-        self.assertEqual(calculadora(5, 0, '^'), 1)
-        self.assertEqual(calculadora(4, 0.5, '^'), 2)
+        self.assertEqual(calcular(2, 3, '^'), 8)
+        self.assertEqual(calcular(5, 0, '^'), 1)
+        self.assertEqual(calcular(4, 0.5, '^'), 2)
 
     def test_operacao_invalida(self):
-        self.assertIsNone(calculadora(5, 3, '%'))
+        self.assertIsNone(calcular(5, 3, '%'))
 
     def test_valores_nao_numericos(self):
-        self.assertIsNone(calculadora('a', 3, '+'))
-        self.assertIsNone(calculadora(5, 'b', '+'))
-        self.assertIsNone(calculadora('a', 'b', '+'))
+        self.assertIsNone(calcular('a', 3, '+'))
+        self.assertIsNone(calcular(5, 'b', '+'))
+        self.assertIsNone(calcular('a', 'b', '+'))
 
 if __name__ == '__main__':
     unittest.main()
